639cf99160448c1398f0b7ac21bea091dfa569c0
Stepan <akumajo27@gmail.com>
2016-08-24 01:39:32 -0400

    everything is working, console version

---
+++ Cell.java
@@ -0,0 +1,78 @@
+
+public class Cell {
+	public static int count = 10;
+	protected int cellState;
+	private boolean mine;
+	private boolean revealed;
+	private boolean marked;
+	
+	public boolean isMarked() {
+		return marked;
+	}
+
+	public void setMarked(boolean marked) {
+		this.marked = marked;
+	}
+
+	public boolean isRevealed() {
+		return revealed;
+	}
+
+	public void setRevealed(boolean revealed) {
+		this.revealed = revealed;
+	}
+
+	public Cell() {
+		cellState = -1;
+		mine = false;
+		revealed =false;
+		marked = false;
+	}
+	
+	public static void setCount(int c) {
+		count = c;
+	}
+	
+	public static void decreaseCount() {
+		count--;
+	}
+	
+	public static int getCount() {
+		return count;
+	}
+	
+	public void setCellState(int s) {
+		cellState = s;
+	}
+	
+	public String getCellState() {
+		switch (cellState) {
+		case -1:
+			return " ☻ ";
+		case 0:
+			return "   ";
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+		case 6:
+		case 7:
+		case 8:
+			return (" "+cellState+" ");
+		case 9:
+			return " ? ";
+		}
+		return " X ";
+	}
+	
+	public void setMine() {
+		mine = true;
+		decreaseCount();
+	}
+	
+	public boolean getMine() {
+		return mine;
+	}
+	
+}
